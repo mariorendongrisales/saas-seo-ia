6d1424b81b7fded288ed75549b090547bca61139
RealignedReality <realignedreality@gmail.com>
2024-11-24 15:10:28 +0000

    Primera versión del proyecto

---
+++ app.py
@@ -0,0 +1,25 @@
+import os
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
+
+@app.route('/services')
+def services():
+    return render_template('services.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+if __name__ == '__main__':
+    # Usar host='0.0.0.0' para permitir conexiones externas (necesario en producción)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5001)))
+
